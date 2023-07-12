3231114ad5dff3ac1b3ae863e28036eb544d7ecc
N1kos9 <x3non66@gmail.com>
2023-07-12 14:11:38 +0300

    Third commit

--- src/components/methods/Methods.jsx
+++ src/components/methods/Methods.jsx
@@ -1,9 +1,49 @@
 import './MethodsStyle.css'
 
 function Methods() {
-  return (
-    <div>Methods</div>
-  )
+  return ( <>
+    <section className='section-3'>
+      <h1 className='main-text-2'>Generating New Customers <br /> Via Online Mode​​</h1>
+      <div className='solutions'>
+        <div className='solution-1'>
+          <div className='icon icon-1'>
+            <img src="/chart.png" alt="" />
+          </div>
+          <div className='text-div'>
+          <h2>All Sizes Business</h2>
+          <p>Seddo eiusmod tempor incididunt labore lore magna aliqua uisipsum suspendis incididun se ultrices gravida.</p>
+          </div>
+        </div>
+        <div className='solution-2'>
+          <div className='icon icon-2'>
+            <img src="/social-media.png" alt="" />
+          </div>
+          <div className='text-div'>
+          <h2>Awesome Results</h2>
+          <p>Seddo eiusmod tempor incididunt labore lore magna aliqua uisipsum suspendis incididun se ultrices gravida.</p>
+          </div>
+        </div>
+        <div className='solution-3'>
+          <div className='icon icon-3'>
+            <img src="/speaker.png" alt="" />
+          </div>
+          <div className='text-div'>
+          <h2>Keep you in the Loop</h2>
+          <p>Seddo eiusmod tempor incididunt labore lore magna aliqua uisipsum suspendis incididun se ultrices gravida.</p>
+         </div>
+        </div>
+        <div className='solution-4'>
+          <div className='icon icon-4'>
+            <img src="/growth.png" alt="" />
+          </div>
+          <div className='text-div'>
+          <h2>Significant ROI</h2>
+          <p>Seddo eiusmod tempor incididunt labore lore magna aliqua uisipsum suspendis incididun se ultrices gravida.</p>
+          </div>
+        </div>
+      </div>
+</section>
+  </>)
 }
 
 export default Methods
